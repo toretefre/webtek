6447ed5ee27ee894bc8f1f4d4933cf905c830ef8
Claus Martinsen <clauswrm@gmail.com>
2016-11-07 10:14:41 +0100

    MODULARITY v0.3

--- js/modularity.js
+++ js/modularity.js
@@ -1,9 +1,10 @@
 //modularity av Claus
-//får footer fra index.html
 const footer = document.getElementById('footer');
+//lager innholdet i <footer> på hver side som kjører skriptet
+function insertFooter() {
+	let footerContent = document.createElement('span');
+	footerContent.innerHTML = 'Laget av studenter på <a href="https://www.ntnu.no/studier/bit">Informatikk ved NTNU i Trondheim</a>.<br><a href="mailto:gravberget@list.stud.ntnu.no" target="_top">Send oss en e-post!</a>';
+	footer.appendChild(footerContent);
+}
 
-//lager en <footer> på hver side som kjører skriptet og
-//inserter samme innhold
-let footerContent = document.createElement('span');
-footerContent.innerHTML = 'Laget av studenter på <a href="https://www.ntnu.no/studier/bit">Informatikk ved NTNU i Trondheim</a>.<br><a href="mailto:gravberget@list.stud.ntnu.no" target="_top">Send oss en e-post!</a>'
-footer.appendChild(footerContent)
+//onload snurr film!
