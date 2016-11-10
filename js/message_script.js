40b53da9f4d77a7b614a87892e94def4823f5a79
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-11 00:00:17 +0100

    fixed the javascript and added dogtags

--- js/message_script.js
+++ js/message_script.js
@@ -1,16 +1,17 @@
 /*
 FILE NAME: message.js
 WRITTEN BY: Trym Høgelid
-WHEN: October 2015
+WHEN: November 2016
 PURPOSE: javascript that autofills email with name, subject and message
 */
+
 function insertMessage() {
-	var name = document.getElementById("insert_name").value;
-	var subject = document.getElementById("insert_subject").value;
-	var message = document.getElementById("insert_message").value;
+	var name = document.getElementById("enter_name").value;
+	var subject = document.getElementById("enter_subject").value;
+	var message = document.getElementById("enter_message").value;
 
 	var body = encodeURI(message)+"%0D%0A"+"%0D%0A"+encodeURI(name);
 
-	window.location.href="mailto:gravberget@list.stud.ntnu.no"+encodeURI(subject)+'&body='+body;
+	window.location.href='mailto:gravberget@list.stud.ntnu.no?subject='+encodeURI(subject)+'&body='+body;
 	return false;
 }
