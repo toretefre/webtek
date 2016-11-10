816719fde17b7c28c86a45de9038ecd025b112de
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-10 20:43:49 +0100

    Added modularity to index page

--- js/modularity.js
+++ js/modularity.js
@@ -1,10 +1,11 @@
 //modularity av Claus
 const footer = document.getElementById('footer');
 //lager innholdet i <footer> på hver side som kjører skriptet
-function insertFooter() {
+window.onload = function insertFooter() {
 	let footerContent = document.createElement('span');
 	footerContent.innerHTML = 'Laget av studenter på <a href="https://www.ntnu.no/studier/bit">Informatikk ved NTNU i Trondheim</a>.<br><a href="mailto:gravberget@list.stud.ntnu.no" target="_top">Send oss en e-post!</a>';
 	footer.appendChild(footerContent);
+	console.log(123123)
 }
 
 //onload snurr film!
