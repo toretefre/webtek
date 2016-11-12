c571ecaaf5c1edfd2aa928c1a538b3b4b08fe8c2
Claus Martinsen <clauswrm@gmail.com>
2016-11-12 15:45:44 +0100

    NAVBAR MODULAR COMPLETE

--- js/modularity.js
+++ js/modularity.js
@@ -6,7 +6,7 @@ PURPOSE: Make footer and nav-bar modular.
 */
 
 const footer = document.getElementById('footer');
-//const navBar = document.getElementById('')
+const navBar = document.getElementById('navbaren');
 
 window.onload = function insertFooter() {
 	let footerContent = document.createElement('span');
@@ -15,6 +15,15 @@ window.onload = function insertFooter() {
 		"<a onclick='jsfunction()' href='#'>Sidekart</a>" +
 		"<br><button class='knappen' id = 'theAnchor' onclick='smoothScroll(\"theAnchor\")'><div class='trekant'></div></button>";
 
+	let navBarContent = document.createElement('ul');
+	navBarContent.className = "navbar";
+	navBarContent.innerHTML = '<li><a href="../html_pages/index.html">Hjem</a></li>' + '<li><a href="../html_pages/om_oss.html">Om oss</a></li>' +
+	'<li><a href="../html_pages/om_gravberget.html">Om Gravberget</a></li>' + '<li class="dropdown">' +
+	'<a href="../html_pages/severdigheter.html" class="dropbtn">Severdigheter</a>' +
+	'<div class="dropdown-content">' + '<a href="../html_pages/bilder.html">Bilder</a>' + '<a href="../html_pages/turloyper.html">Turløyper</a>' +
+	'<a href="../html_pages/slektstre.html">Slektstre</a>' + '</div>' + '</li>' +'<li><a href="../html_pages/kontakt_oss.html">Kontakt Oss</a></li>';
+
 	footer.appendChild(footerContent);
-	console.log(123123)
+	navBar.appendChild(navBarContent);
+	console.log('Modularitet kjører');
 };
