785af98736eed5aba0b0088f4b546c5b95488357
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-13 18:50:20 +0100

    minor change

--- js/sitemap'.js
+++ js/sitemap'.js
@@ -2,8 +2,8 @@
  * Created by toret on 12.11.2016.
  */
 var btn = document.getElementById("button");
+var img = document.createElement("IMG");
 function loadSitemap() {
-	var img = document.createElement("IMG");
 	img.src = "../images/dugnad11.jpg"
 	img.width = 200;
 	img.height = 200;
