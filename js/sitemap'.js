c2809d93bea09de5eaadd18943184ab90f21c8d7
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-13 18:48:15 +0100

    Sitemap fix

--- js/sitemap'.js
+++ js/sitemap'.js
@@ -1,17 +1,13 @@
 /**
  * Created by toret on 12.11.2016.
  */
-var btn = document.getElementById('btn');
-var modal = document.getElementById('myModal');
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-btn.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
+var btn = document.getElementById("button");
+function loadSitemap() {
+	var img = document.createElement("IMG");
+	img.src = "../images/dugnad11.jpg"
+	img.width = 200;
+	img.height = 200;
+	img.alt = "Sitemap";
 
-}
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function closeModal() {
-	modal.style.display = "none";
+	footer.appendChild(img);
 }
