376a9de430cc412061cfe4ade49be43545ad8fcf
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-13 19:10:48 +0100

    Removed the fnutt and some changes to the script

---
+++ js/sitemap.js
@@ -0,0 +1,27 @@
+/**
+ * Created by toret on 12.11.2016.
+ */
+var btn = document.getElementById("button");
+var img = document.createElement("IMG");
+var closeButton = document.createElement("BUTTON");
+var text = document.createTextNode("Lukk sitemap");
+closeButton.appendChild(text);
+closeButton.id = "closeSitemap";
+closeButton.class = "knappen";
+closeButton.style.display = "none";
+function loadSitemap() {
+	img.src = "../images/dugnad11.jpg"
+	img.width = 200;
+	img.height = 200;
+	img.alt = "Sitemap";
+	img.style.display= "block";
+	footer.appendChild(img);
+	footer.appendChild(closeButton);
+	closeButton.style.display = "block";
+	closeButton.onclick = function() {
+		img.style.display = "none";
+		closeButton.style.display = "none";
+
+	}
+
+}
