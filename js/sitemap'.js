bde565b4e69dbd36201ee7090ae4f7ce3c69263e
trymhog97 <tehogeli@stud.ntnu.no>
2016-11-13 18:30:27 +0100

    sitemap shit

--- js/sitemap'.js
+++ js/sitemap'.js
@@ -1,3 +1,17 @@
 /**
  * Created by toret on 12.11.2016.
  */
+var btn = document.getElementById('btn');
+var modal = document.getElementById('myModal');
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function closeModal() {
+	modal.style.display = "none";
+}
